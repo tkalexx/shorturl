62002510bcf6281a6c60eddba59de6e55ec897ac
Татьяна Чернышова <Ta.Chernyshova@x5.ru>
2026-07-10 23:44:32 +0300

    refactor: Правка файла миграции

--- migrations/000001_create_urls_table.sql
+++ migrations/000001_create_urls_table.sql
@@ -1,7 +1,9 @@
+-- +goose Up
 CREATE TABLE IF NOT EXISTS urls (
                                     id VARCHAR(255) PRIMARY KEY,
     original_url TEXT NOT NULL UNIQUE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
 
+-- +goose Down
 DROP TABLE IF EXISTS urls;
